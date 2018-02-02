53a9af9564e94886a28c62bc85078e4eca74335a
Yi-Ke Peng <yi.ke.peng@gmail.com>
2018-02-01 21:53:47 -0800

    Add test for step_north

--- chessai/ai/bitboard.pyx
+++ chessai/ai/bitboard.pyx
@@ -28,9 +28,9 @@ cdef extern from "bitboardlib.h":
         r
         p
         ep
-        
+
     ctypedef unsigned char brdidx
-    
+
     ctypedef struct boardstate:
         bitboard k
         bitboard q
@@ -49,7 +49,7 @@ cdef extern from "bitboardlib.h":
         unsigned int halfmove_clock
         unsigned int fullmove_counter
         piece piece_map[64]
-    
+
     ctypedef struct move:
         brdidx from_square
         brdidx to_square
@@ -60,7 +60,7 @@ cdef extern from "bitboardlib.h":
         bool lost_castle_queen
         brdidx from_square
         brdidx to_square
-    
+
     cdef moverecord make_move(boardstate *brd, move *mv);
     cdef const boardstate emptyboardstate
     cdef bool get_white_castle_king(boardstate *bs)
@@ -98,6 +98,7 @@ cdef extern from "bitboardlib.h":
     cdef bitboard slide_northeast(bitboard pieces, bitboard unoccupied)
     cdef bitboard slide_southeast(bitboard pieces, bitboard unoccupied)
     cdef bitboard slide_southwest(bitboard pieces, bitboard unoccupied)
+    cdef bitboard step_north(bitboard bb)
     cdef bitboard ls1b(bitboard b)
     cdef int greatest_square_index(bitboard b)
     cdef int greatest_rank_index(bitboard b)
@@ -107,7 +108,7 @@ cdef extern from "bitboardlib.h":
     cdef bitboard bitboard_from_square_index(int i)
     cdef void unplace_piece(boardstate *bs, brdidx square_index)
     cdef void place_piece(boardstate *bs, brdidx square_index, piece pc)
-    
+
 cpdef bitboard_to_str(bitboard bb):
     cdef int i
     result = ''
@@ -120,7 +121,7 @@ cpdef bitboard_to_str(bitboard bb):
 
 cdef class BitBoard:
     cdef bitboard bb
-    
+
     def __richcmp__(BitBoard self, BitBoard other, int op):
         if op != Py_EQ:
             return NotImplemented()
@@ -128,14 +129,14 @@ cdef class BitBoard:
             return True
         else:
             return False
-    
+
     @classmethod
     def from_square_index(cls, int idx):
         cdef bitboard bb = bitboard_from_square_index(idx)
         cdef BitBoard result = BitBoard()
         result.bb = bb
         return result
-        
+
     @classmethod
     def from_str(cls, str s):
         cdef bitboard bb = empty
@@ -145,64 +146,69 @@ cdef class BitBoard:
         cdef BitBoard result = BitBoard()
         result.bb = bb
         return result
-    
+
     @classmethod
     def from_grid(cls, str g):
         cdef list lines = list(map(methodcaller('strip'), g.strip().split()))
         cdef str s = ''.join(reversed(''.join(reversed(lines))))
         return cls.from_str(s)
-    
+
     def to_str(self):
         return bitboard_to_str(self.bb)
-    
+
     def to_grid(self):
         return '\n'.join(reversed(map(''.join, partition(8, reversed(self.to_str())))))
-    
+
     cpdef BitBoard ls1b(BitBoard self):
         cdef BitBoard result = BitBoard()  # @DuplicatedSignature
         result.bb = ls1b(self.bb)
         return result
-    
+
     cpdef BitBoard slide_north(BitBoard self, BitBoard unoccupied):
         cdef BitBoard result = BitBoard()  # @DuplicatedSignature
         result.bb = slide_north(self.bb, unoccupied.bb)
         return result
-    
+
     cpdef BitBoard slide_south(BitBoard self, BitBoard unoccupied):
         cdef BitBoard result = BitBoard()  # @DuplicatedSignature
         result.bb = slide_south(self.bb, unoccupied.bb)
         return result
-    
+
     cpdef BitBoard slide_east(BitBoard self, BitBoard unoccupied):
         cdef BitBoard result = BitBoard()  # @DuplicatedSignature
         result.bb = slide_east(self.bb, unoccupied.bb)
         return result
-    
+
     cpdef BitBoard slide_west(BitBoard self, BitBoard unoccupied):
         cdef BitBoard result = BitBoard()  # @DuplicatedSignature
         result.bb = slide_west(self.bb, unoccupied.bb)
         return result
-    
+
     cpdef BitBoard slide_northeast(BitBoard self, BitBoard unoccupied):
         cdef BitBoard result = BitBoard()  # @DuplicatedSignature
         result.bb = slide_northeast(self.bb, unoccupied.bb)
         return result
-    
+
     cpdef BitBoard slide_northwest(BitBoard self, BitBoard unoccupied):
         cdef BitBoard result = BitBoard()  # @DuplicatedSignature
         result.bb = slide_northwest(self.bb, unoccupied.bb)
         return result
-    
+
     cpdef BitBoard slide_southeast(BitBoard self, BitBoard unoccupied):
         cdef BitBoard result = BitBoard()  # @DuplicatedSignature
         result.bb = slide_southeast(self.bb, unoccupied.bb)
         return result
-    
+
     cpdef BitBoard slide_southwest(BitBoard self, BitBoard unoccupied):
         cdef BitBoard result = BitBoard()  # @DuplicatedSignature
         result.bb = slide_southwest(self.bb, unoccupied.bb)
         return result
-    
+
+    cpdef BitBoard step_north(BitBoard self):
+        cdef BitBoard result = BitBoard()  # @DuplicatedSignature
+        result.bb = step_north(self.bb)
+        return result
+
 def compress_row(row):
     result = ''
     count = 0
@@ -291,47 +297,47 @@ cdef class Move:
         mv.from_square = from_square
         mv.to_square = to_square
         self.mv = mv
-    
+
 cdef class MoveRecord:
     cdef moverecord rec
     property from_square:
         def __get__(MoveRecord self):
             return self.rec.from_square
-#         
+#
 #         def __set__(MoveRecord self, int square_index):
 #             self.rec.from_square = square_index
-        
+
     property to_square:
         def __get__(MoveRecord self):  # @DuplicatedSignature
             return self.rec.to_square
-        
+
 #         def __set__(MoveRecord self, int square_index):  # @DuplicatedSignature
 #             self.rec.to_square = square_index
-        
+
     property captured:
         def __get__(MoveRecord self):  # @DuplicatedSignature
             return piece_to_str(self.rec.captured)
-        
+
     property lost_castle_king:
         def __get__(MoveRecord self):  # @DuplicatedSignature
             if self.rec.lost_castle_king:
                 return True
             else:
                 return False
-    
+
     property lost_castle_queen:
         def __get__(MoveRecord self):  # @DuplicatedSignature
             if self.rec.lost_castle_queen:
                 return True
             else:
                 return False
-        
+
 cdef class BitBoardState:
     cdef boardstate bs
-    
+
     cpdef place_piece(BitBoardState self, int idx, str pc):
         place_piece(&(self.bs), <brdidx> idx, str_to_piece(pc))
-    
+
     @classmethod
     def from_fen(cls, str fen):
         cdef boardstate bs
@@ -339,7 +345,7 @@ cdef class BitBoardState:
         cdef BitBoardState result = BitBoardState()
         result.bs = bs
         return result
-    
+
     cpdef MoveRecord make_move(BitBoardState self, Move mv):
         cdef moverecord rec
         rec = make_move(&(self.bs), &(mv.mv))
@@ -347,7 +353,7 @@ cdef class BitBoardState:
         result = MoveRecord()
         result.rec = rec
         return result
-    
+
     cpdef to_grid_redundant(BitBoardState self):
         '''
         Use the redundant piece_map instead of the bitboards.
@@ -364,7 +370,7 @@ cdef class BitBoardState:
                 c = '*'
             result = result + c
         return '\n'.join(reversed(map(''.join, partition(8, result))))
-    
+
     cpdef str to_grid(BitBoardState self):
         # Get raw positions
         cdef char *arr = <char *>PyMem_Malloc(64 * sizeof(char))
@@ -375,25 +381,25 @@ cdef class BitBoardState:
             ins = '*'
             raw_positions = raw_positions[:(self.bs.enpassant)] + ins + raw_positions[(self.bs.enpassant+1):]
         return '\n'.join(reversed(map(''.join, partition(8, raw_positions))))
-    
+
     cpdef str to_fen(BitBoardState self):
-        
+
         # Get raw positions
         cdef char *arr = <char *>PyMem_Malloc(64 * sizeof(char))  # @DuplicatedSignature
         bitboard_to_arr(&(self.bs), arr)
         raw_positions = str(arr)
         PyMem_Free(arr)
-        
+
         # Compress positions into fen format
         rows = list(map(''.join, partition(8, raw_positions)))
         for i in range(len(rows)):
             rows[i] = compress_row(rows[i])
         fen_positions = '/'.join(reversed(rows))
-        
+
         # Get turn
         cdef bool whites_turn = get_whites_turn(&(self.bs))
         turn = 'w' if whites_turn else 'b'
-        
+
         # Get castles
         cdef bool white_castle_king = get_white_castle_king(&(self.bs))
         cdef bool white_castle_queen = get_white_castle_queen(&(self.bs))
@@ -406,61 +412,61 @@ cdef class BitBoardState:
         castles += 'q' if black_castle_queen else ''
         if not castles:
             castles = '-'
-        
+
         # Get en passant
         cdef int en_passant_int = get_enpassant(&(self.bs))
         en_passant = int_to_algebraic(en_passant_int) if en_passant_int != 255 else '-'
-        
+
         # Get clocks and counts
         cdef int halfmove_clock = get_halfmove_clock(&(self.bs))
         cdef int fullmove_count = get_fullmove_counter(&(self.bs))
-        
+
         return ' '.join([fen_positions, turn, castles, en_passant, str(halfmove_clock), str(fullmove_count)])
-    
+
     cpdef BitBoard get_k(BitBoardState self):
         result = BitBoard()
         result.bb =  self.bs.k
         return result
-        
+
     cpdef BitBoard get_q(BitBoardState self):
         result = BitBoard()
         result.bb =  self.bs.q
         return result
-    
+
     cpdef BitBoard get_b(BitBoardState self):
         result = BitBoard()
         result.bb =  self.bs.b
         return result
-    
+
     cpdef BitBoard get_r(BitBoardState self):
         result = BitBoard()
         result.bb =  self.bs.r
         return result
-    
+
     cpdef BitBoard get_n(BitBoardState self):
         result = BitBoard()
         result.bb =  self.bs.n
         return result
-    
+
     cpdef BitBoard get_p(BitBoardState self):
         result = BitBoard()
         result.bb =  self.bs.p
         return result
-    
+
     cpdef BitBoard get_white(BitBoardState self):
         result = BitBoard()
         result.bb =  self.bs.white
         return result
-    
+
     cpdef BitBoard get_black(BitBoardState self):
         result = BitBoard()
         result.bb =  self.bs.black
         return result
-    
+
     cpdef int get_enpassant(BitBoardState self):
         cdef int result = self.bs.enpassant
         return result
-    
+
 cpdef int algebraic_to_int(str alg):
     return ord(alg[0].lower()) - ord('a') + 8 * (int(alg[1]) - 1)
 
@@ -474,9 +480,9 @@ cdef str bb_to_grid(bitboard bb):
 
 cdef boardstate fen_to_bitboard(str fen):
     cdef boardstate bs = emptyboardstate;
-    cdef str pieces, turn, castles, en_passant, halfmove_clock, move_number 
+    cdef str pieces, turn, castles, en_passant, halfmove_clock, move_number
     pieces, turn, castles, en_passant, halfmove_clock, move_number = fen.split(' ')
-    
+
     cdef int pos = 0
     cdef str ch
     ranks = list(reversed(pieces.split('/')))
@@ -487,12 +493,12 @@ cdef boardstate fen_to_bitboard(str fen):
             else:
                 place_piece(&bs, pos, str_to_piece(ch))
                 pos += 1
-    
+
     if turn.lower() == 'w':
         set_whites_turn(&bs)
     else:
         set_blacks_turn(&bs)
-    
+
     if 'K' in castles:
         set_white_castle_king(&bs)
     if 'k' in castles:
@@ -501,7 +507,7 @@ cdef boardstate fen_to_bitboard(str fen):
         set_white_castle_queen(&bs)
     if 'q' in castles:
         set_black_castle_queen(&bs)
-    
+
     if en_passant != '-':
         set_enpassant(&bs, algebraic_to_int(en_passant))
         if bs.whites_turn:
@@ -510,6 +516,5 @@ cdef boardstate fen_to_bitboard(str fen):
             bs.piece_map[algebraic_to_int(en_passant)] = EP
     set_halfmove_clock(&bs, int(halfmove_clock))
     set_fullmove_counter(&bs, int(move_number))
-    
+
     return bs
-            
